23c28da7b3ec0898faf7b879d30a36bc9f6a0317
1100110010 <75100630+1100110010@users.noreply.github.com>
2024-07-27 21:57:06 +0300

    Update anabetik.py

--- anabetik.py
+++ anabetik.py
@@ -2,12 +2,9 @@ from os import system
 from time import sleep
 import sys
 
-#system("clear")
-def exit_program():
-    print("Exiting the program...")
-    sys.exit(0)
-
-sleep(3)
+sleep(1)
+system("clear")
+sleep(1)
 line_1 = """Merhabalar.""" + "\n"
 for x in line_1:
     print(x, end='')
@@ -15,21 +12,21 @@ for x in line_1:
     sleep(0.02)
 sleep(0.8)
 
-line_2 = """Lütfen programı yönetici izinleri ile başlatıp, terminali tam ekran yapmayı unutmayın.""" + "\n"
+line_2 = """Lütfen programı yönetici izinleri ile başlatmayı unutmayın.""" + "\n"
 for x in line_2:
     print(x, end='')
     sys.stdout.flush()
     sleep(0.02)
 sleep(0.8)
 
-line_3 = """F11'e tıklamanıza gerek yok."""
+line_3 = """Programı tamamen izlemenize, takip etmenize gerek yok.""" + "\n"
 for x in line_3:
     print(x, end='')
     sys.stdout.flush()
     sleep(0.02)
 sleep(0.4)
 
-line_3 = """ Ekranı büyütüp bütün ekranı kaplamasını sağlamanız yeterli olacaktır.""" + "\n"
+line_3 = """Seçim anlarında tercih vermeniz yeterli olacaktır.""" + "\n"
 for x in line_3:
     print(x, end='')
     sys.stdout.flush()
@@ -43,15 +40,65 @@ for x in line_3:
     sleep(0.02)
 sleep(1)
 
-line_4 = " <3"
+line_4 = " <3" + "\n"
 for x in line_4:
     print(x, end='')
     sys.stdout.flush()
     sleep(0.02)
-sleep(0.5)
-print("\n")
-soru = input("Programı gerekli koşullar altında başlattınız mı? (E/H")
-if not soru ==  "e"  or "E" or "y" or "Y":
-    exit_program()
+sleep(2)
+input("Devam etmek için 'enter' tuşuna basınız: ")
+system("clear")
+soru = input("Sistemi güncellemek ister misiniz? (E/H): ")
+if soru == "e":
+    sistem = 1
+elif soru == "E":
+    sistem = 1
+elif soru == "y":
+    sistem = 1
+elif soru == "Y":
+    sistem = 1
 
-print("deneme")
+system("clear")
+soru = input("Brave tarayıcısını indirmek ister misiniz? (E/H): ")
+if soru == "e":
+    brave = 1
+elif soru == "E":
+    brave = 1
+elif soru == "y":
+    brave = 1
+elif soru == "Y":
+    brave = 1
+
+system("clear")
+soru = input("Firefox tarayıcısını silmek ister misiniz? (E/H): ")
+if soru == "e":
+    firefox = 1
+elif soru == "E":
+    firefox = 1
+elif soru == "y":
+    firefox = 1
+elif soru == "Y":
+    firefox = 1
+
+system("clear")
+soru = input("""Tuxedo Yönetim Merkezi indirme betiği varsayılan olarak Jammy sürümüne ayarlıdır.
+Eğer Jammy sürümü kullanmıyorsanız dosyalardan betiği kendinizce ayarlayabilir,
+veya dilerseniz bu etabı atlayıp işlemi elle gerçekleştirebilirsiniz.
+Tuxedo Yönetim Merkezini indirmek ister misiniz? (E/H): """)
+if soru == "e":
+    tuxedo = 1
+elif soru == "E":
+    tuxedo = 1
+elif soru == "y":
+    tuxedo = 1
+elif soru == "Y":
+    tuxedo = 1
+system("clear")
+if sistem == 1:
+    system("cd betikler && bash sistem.sh")
+if brave == 1:
+    system("cd betikler && bash tarayıcı.sh")
+if firefox == 1:
+    system("cd betikler && bash firefox.sh")
+if tuxedo == 1:
+    system("cd betikler && bash tuxedo.sh")
